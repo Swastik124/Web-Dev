b88285bba09f7f7e7ed371aae1da5f1b9695dcfd
Swastik124 <swastikmohanty124@gmail.com>
2024-07-12 12:11:47 +0530

    There's some change

--- React/todo/src/App.jsx
+++ React/todo/src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react"
 import {useState} from 'react'
+import TodoComp from "./componenets/TodoComp"
 
 function App() {
   const [todo,setTodo]= useState("");
@@ -7,18 +8,40 @@ function App() {
 
 
   function pushData(){
+    let todoObj={
+      id:Math.random()+Date.now()+todo,
+      todo:todo,
+      isCompleted:false
+    }
     setTodoArr([...todoArr,todo]);
     console.log(todoArr);
   }
 
+  function handleDel(id){
+    console.log(id);
+    let newTodoArr= todoArr.filter((el)=>el.id !== id);
+    setTodoArr(newTodoArr);
+  }
+  function update(id){
+    let updatedArr = todoArr.map((el)=>{
+      el.id === id ? {...el,isCompleted :  !el.isCompleted} : el;
+    })
+    console.log(updatedArr);
+    setTodoArr(updatedArr);
+  }
   
   return (
     <div>
-      <input type="text" id="input" placeholder='Todo Input' onChange={(e)=>{setTodo(e.target.value)}} />
+      <input type="text" id="" placeholder='Todo Input' onChange={(e)=>{setTodo(e.target.value)}} />
       <button onClick={pushData}>Click</button>
       {
-        todoArr.map((e1)=>(
-          <h1>{e1}</h1>
+        todoArr.map((el)=>(
+          <div>
+            <h1>{el.todo}</h1>
+            <h1>{el.isCompleted? "Completed" : "Not Completed"}</h1>
+            <button onClick={()=>{handleDel(el.id)}}>Delete</button>
+            <button onClick={()=>{update(el.id)}}>Update Task</button>
+          </div>
         ))
       }
     </div>
